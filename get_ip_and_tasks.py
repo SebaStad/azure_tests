071b40b81608996eea3a3854f18afdf564d98835
Seb <SebaStad@users.noreply.github.com>
2021-10-21 16:07:34 +0200

    Create get_ip_and_tasks.py

---
+++ get_ip_and_tasks.py
@@ -0,0 +1,14 @@
+import os
+import subprocess
+
+bash_command = "nmap -n -sn 10.0.0.0/24 -oG - | awk '/Up$/{print $2}'"
+ip_addresses = subprocess.check_output(["bash", "-c", bash_command])
+
+cores_per_node = 2
+ip_list = ip_addresses.decode("utf-8").split("\n")
+ip_list_ne = [item for item in ip_list if len(item)!=0]
+
+ip_plus_cpn = [item + f":{cores_per_node}" for item in ip_list_ne]
+mpi_host_settings = ","join(ip_plus_cpn)
+
+os.environ("MPI_HOST_SETTINGS") = mpi_host_settings
