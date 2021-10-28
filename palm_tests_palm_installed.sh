03be2dcd4ea3bb78071d5b70be57706526a78c4d
Seb <SebaStad@users.noreply.github.com>
2021-10-28 15:57:27 +0200

    Update palm_tests_palm_installed.sh

--- palm_tests_palm_installed.sh
+++ palm_tests_palm_installed.sh
@@ -25,7 +25,7 @@ echo "$command_option"
 cd /palmbase/palm/
 
 echo "Starting palm"
-./bin/palmrun -a "d3#" -X 1 -r example_cbl
+./bin/palmrun -a "d3#" -X 2 -r example_cbl
 
 echo "Simulation results"
 filepath_results=$(ls palmbase/palm/JOBS/example_cbl/OUTPUT)
